553686c190e4dda3379b4b45a201a7521f3692f7
klaranit <klarapionit@gmail.com>
2025-12-23 17:24:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,15 @@
   // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
   $(window).on('load hashchange', function(){
     
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').hide();
+    // First hide all content regions by adding the hide class
+    $('.content-region').addClass('hide');
     
     // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
     var region = location.hash.toString() || $('.main-menu a:first').attr('href');
     
-    // Now show the region specified in the URL hash
-    $(region).show();
+    // Now show the region specified in the URL hash by removing the hide class
+    $(region).removeClass('hide');
     
     // Highlight the menu link associated with this region by adding the .active CSS class
     $('.main-menu a[href="'+ region +'"]').addClass('active'); 
@@ -22,24 +21,23 @@
   });
   
   // Handle project card clicks
-  $('.project-card').on('click', function() {
-    var projectId = $(this).attr('id');
+  $(document).on('click', '.project-card', function() {
+    var projectId = $(this).attr('data-project');
     
-    // Hide all content regions
-    $('.content-region').hide();
+    // Hide all content regions by adding the hide class
+    $('.content-region').addClass('hide');
     
-    // Show the specific project detail page
-    $('#project-' + projectId).show();
+    // Show the specific project detail page by removing the hide class
+    $('#project-' + projectId).removeClass('hide');
     
-    // Update the URL hash (optional, for back button support)
+    // Update the URL hash (for back button support)
     location.hash = '#project-' + projectId;
   });
   
   // Handle back button clicks
-  $('.back-button').on('click', function(e) {
+  $(document).on('click', '.back-button', function(e) {
     e.preventDefault();
     location.hash = '#projects';
   });
   
-})(jQuery);
-    
+})(jQuery);   
