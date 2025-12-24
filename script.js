b4e60c01670629d51b1cefcc9c97aa51df6d858d
klaranit <klarapionit@gmail.com>
2025-12-24 14:05:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 (function ($) {
   
   // We use some Javascript and the URL #fragment to hide/show different parts of the page
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
   $(window).on('load hashchange', function(){
     
     // First hide all content regions by adding the hide class
@@ -9,9 +10,7 @@
     
     // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
-    
-    // Get the current hash or default to first menu item
-    var region = location.hash.toString() || '#aboutme';
+    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
     
     // Now show the region specified in the URL hash by removing the hide class
     $(region).removeClass('hide');
@@ -19,30 +18,46 @@
     // Highlight the menu link associated with this region by adding the .active CSS class
     $('.main-menu a[href="'+ region +'"]').addClass('active'); 
     
+    // When navigating away from projects, reset the view
+    if (region !== '#projects') {
+      $('.projects-grid').show();
+      $('#project-details-section').hide();
+    }
+    
   });
   
   // Handle project card clicks
-  $(document).on('click', '.project-card', function(e) {
-    e.preventDefault();
+  $(document).on('click', '.project-card', function() {
     var projectId = $(this).attr('data-project');
     
-    // Hide all content regions
-    $('.content-region').addClass('hide');
+    // Hide the projects grid
+    $('.projects-grid').hide();
     
-    // Show the specific project detail page
-    $('#project-' + projectId).removeClass('hide');
+    // Show the project details section
+    $('#project-details-section').show();
     
-    // Remove active class from menu
-    $('.main-menu a').removeClass('active');
+    // Hide all project details
+    $('.project-detail').hide();
     
-    // Update the URL hash
-    window.location.hash = 'project-' + projectId;
+    // Show the specific project detail
+    $('.project-detail[data-detail="' + projectId + '"]').show();
+    
+    // Scroll to top
+    $('html, body').animate({ scrollTop: 0 }, 300);
   });
   
   // Handle back button clicks
   $(document).on('click', '.back-button', function(e) {
     e.preventDefault();
-    window.location.hash = 'projects';
+    
+    // Hide project details section
+    $('#project-details-section').hide();
+    
+    // Show projects grid
+    $('.projects-grid').show();
+    
+    // Scroll to top
+    $('html, body').animate({ scrollTop: 0 }, 300);
   });
   
 })(jQuery);
